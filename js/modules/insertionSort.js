81333cc8eecd8ca2ea7020ff9cee1222f4b01fce
João Vítor S. F <53799823+jjoaovitor7-unit@users.noreply.github.com>
2020-09-30 12:43:02 -0300

    Create insertionSort.js

---
+++ js/modules/insertionSort.js
@@ -0,0 +1,28 @@
+function insertionSort(arr) {
+  let qtde = arr.length;
+  let current;
+  let i, j;
+
+  for (i = 1; i < qtde; i++) {
+    current = arr[i];
+    j = i - 1;
+
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = current;
+  }
+
+  return arr;
+}
+
+function insertionSortRuntime(arr) {
+  let begin = Date.now();
+  ordered.textContent = insertionSort(arr);
+  let end = Date.now();
+  let time = (end - begin) * 1000000;
+  runtime.textContent = "Tempo de Execução: " + time + "ns";
+}
+
+export default insertionSortRuntime;
