18520156cd081c5d77d46d93c334289f46abd6df
João Vítor S. F <53799823+jjoaovitor7-unit@users.noreply.github.com>
2020-10-20 20:34:51 -0300

    Update quickSort.js

--- js/modules/quickSort.js
+++ js/modules/quickSort.js
@@ -22,8 +22,8 @@ function quickSortRuntime(arr) {
   let begin = Date.now();
   ordered.textContent = quickSort(arr);
   let end = Date.now();
-  let timeNS = (end - begin) * 1000000000;
-  let timeS = (end - begin) / 1000000000;
+  let timeNS = (end - begin) * 1000000;
+  let timeS = (end - begin) / 1000;
   runtime.textContent = "Tempo de Execução: " + timeNS + "ns ou " + timeS + "s.";
 }
 
