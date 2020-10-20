8169834fd17d3dddb58298be4ac4f94b7c6dd2aa
João Vítor S. F <53799823+jjoaovitor7-unit@users.noreply.github.com>
2020-10-20 17:25:02 -0300

    Update selectionSort.js

--- js/modules/selectionSort.js
+++ js/modules/selectionSort.js
@@ -22,8 +22,9 @@ function selectionSortRuntime(arr) {
   let begin = Date.now();
   ordered.textContent = selectionSort(arr);
   let end = Date.now();
-  let time = (end - begin) * 1000000;
-  runtime.textContent = "Tempo de Execução: " + time + "ns";
+  let timeNS = (end - begin) * 1000000000;
+  let timeS = (end - begin) / 1000000000;
+  runtime.textContent = "Tempo de Execução: " + timeNS + "ns ou " + timeS + "s.";
 }
 
 export default selectionSortRuntime;
