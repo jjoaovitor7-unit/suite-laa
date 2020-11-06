2c99dbb9925459427c398989d5ba36c0a131bf39
João Vítor <joao.sferreira@souunit.com.br>
2020-11-06 13:53:03 -0300

    organização.

--- index.js
+++ index.js
@@ -4,18 +4,25 @@ window.onload = () => {
 
 let module = {
   renderHomePage: () => {
+    // body
     let body = document.querySelector("body");
+
+
+    // #page-lading
     let divPageLanding = document.createElement("div");
     divPageLanding.setAttribute("id", "page-landing");
+    body.appendChild(divPageLanding);
 
+
+    // header
     let header = document.createElement("header");
     let h1Header = document.createElement("h1");
     h1Header.innerHTML = 'Suíte <span class="highlight">LAA</span>';
-
     header.appendChild(h1Header);
     divPageLanding.appendChild(header);
-    body.appendChild(divPageLanding);
 
+
+    // .container-input
     divElements = document.createElement("div");
     divElements.classList = "container-input";
     divPageLanding.appendChild(divElements);
@@ -29,16 +36,20 @@ let module = {
     inputElements.setAttribute("id", "elements");
     divElements.appendChild(inputElements);
 
+    // #btn-ordenar
     buttonElements = document.createElement("button");
     buttonElements.type = "submit";
     buttonElements.setAttribute("id", "btn-ordenar");
     buttonElements.innerHTML = "Ordenar!";
     divElements.appendChild(buttonElements);
 
+
+    // .radios-options
     divRadios = document.createElement("div");
     divRadios.classList = "radios-options";
     divPageLanding.appendChild(divRadios);
 
+    // Insertion Sort
     radioInsertionSort = document.createElement("input");
     radioInsertionSort.type = "radio";
     radioInsertionSort.setAttribute("name", "sort");
@@ -49,6 +60,7 @@ let module = {
     labelInsertionSort.innerHTML = "Insertion Sort";
     divRadios.appendChild(labelInsertionSort);
 
+    // Bubble Sort
     radioBubbleSort = document.createElement("input");
     radioBubbleSort.type = "radio";
     radioBubbleSort.setAttribute("name", "sort");
@@ -59,6 +71,7 @@ let module = {
     labelBubbleSort.innerHTML = "Bubble Sort";
     divRadios.appendChild(labelBubbleSort);
 
+    // Selection Sort
     radioSelectionSort = document.createElement("input");
     radioSelectionSort.type = "radio";
     radioSelectionSort.setAttribute("name", "sort");
@@ -69,6 +82,7 @@ let module = {
     labelSelectionSort.innerHTML = "Selection Sort";
     divRadios.appendChild(labelSelectionSort);
 
+    // Quick Sort
     radioQuickSort = document.createElement("input");
     radioQuickSort.type = "radio";
     radioQuickSort.setAttribute("name", "sort");
@@ -79,19 +93,23 @@ let module = {
     labelQuickSort.innerHTML = "Quick Sort";
     divRadios.appendChild(labelQuickSort);
 
+    // .obs
     pObs = document.createElement("p");
     pObs.classList = "obs";
     pObs.innerHTML = "Obs.: Separar os elementos com vírgula.";
     divPageLanding.appendChild(pObs);
 
+    // #ordered
     pOrdered = document.createElement("p");
     pOrdered.setAttribute("id", "ordered");
     divPageLanding.appendChild(pOrdered);
 
+    // #runtime
     pRuntime = document.createElement("p");
     pRuntime.setAttribute("id", "runtime");
     divPageLanding.appendChild(pRuntime);
 
+    // script
     scriptMain = document.createElement("script");
     scriptMain.type = "module";
     scriptMain.src = "./public/scripts/main.js";
